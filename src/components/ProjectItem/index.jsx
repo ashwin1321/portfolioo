6c038c261ed3f6dfc6324f97936923e48ceafa7b
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-04-12 15:14:32 +0545

    project section complete

--- src/components/ProjectItem/index.jsx
+++ src/components/ProjectItem/index.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import Styles from './Project.module.css'
 import { FaGithub, FaLink } from 'react-icons/fa'
 
-const index = ({ img, name, desc }) => {
+const index = ({ img, name, desc, git, link }) => {
     return (
         <div className={Styles.card}>
             <div className={Styles.content}>
                 <img src={img} alt="project" className={Styles.img} />
                 <h1 className={Styles.name}>{name}</h1>
                 <div className={Styles.links}>
-                    <a href="github.com" className={Styles.link}>
+                    <a href={git} className={Styles.link}>
                         <FaGithub size={23} />
                     </a>
-                    <a href="github.com" className={Styles.link}>
+                    <a href={link} className={Styles.link}>
                         <FaLink size={23} />
                     </a>
 
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,12 +1,54 @@
 import React from 'react'
 import Styles from './Projects.module.css'
 import ProjectItem from '../../components/ProjectItem'
+import { js, react, node, paisa, college, hospital, crowd, boy } from '../../assets/images'
 
 
 const index = ({ projectRef }) => {
     return (
-        <div ref={projectRef}>
-            <ProjectItem />
+        <div ref={projectRef} className={Styles.projectSection}>
+            <div className={Styles.head}>
+                Projects
+            </div>
+            <div className={Styles.projects}>
+                <ProjectItem
+                    img={college}
+                    name="College Management System"
+                    desc='this is a project1'
+                    git="https://github.com/ashwin1321/manage-college"
+                    link="https://github.com/ashwin1321/manage-college"
+                    className={Styles.project} />
+
+                <ProjectItem
+                    img={hospital}
+                    name="Hospital Management System"
+                    desc='this is a project1'
+                    git="https://github.com/ashwin1321/Hospital-Management-System"
+                    link="https://github.com/ashwin1321/Hospital-Management-System"
+                    className={Styles.project} />
+
+                <ProjectItem img={paisa}
+                    name="Expense Tracker App"
+                    desc='this is a project1'
+                    git="https://github.com/ashwin1321/expense-tracker"
+                    link="https://paisakhoi.cyclic.app/login"
+                    className={Styles.project} />
+
+                <ProjectItem img={crowd}
+                    name="Crowdfunding App"
+                    desc='this is a project1'
+                    git="https://github.com/ashwin1321/CrowdFundingDapp"
+                    link="https://ashwin-crowdfund.netlify.app/"
+                    className={Styles.project} />
+
+                <ProjectItem img={boy}
+                    name="Ashwin Portfolio Site"
+                    desc='this is a project1'
+                    git="https://github.com/ashwin1321/portfolioo"
+                    link="https://github.com/ashwin1321/portfolioo"
+                    className={Styles.project} />
+            </div>
+
         </div>
     )
 }
