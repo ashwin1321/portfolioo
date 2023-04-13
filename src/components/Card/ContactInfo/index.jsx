cd557d28c933b7d09a076af4dab98d435dcca035
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-04-13 09:58:22 +0545

    feat: contact page section created with address and social icons

--- src/components/Card/ContactInfo/index.jsx
+++ src/components/Card/ContactInfo/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Style from './ContactInfo.module.css'
+import { AiOutlineMobile, AiFillHome } from "react-icons/ai";
+import { SiGmail } from "react-icons/si";
 
-const index = () => {
+
+const index = ({ icon, value }) => {
     return (
-        <div>index</div>
+        <div className={Style.contactCard}>
+            <div className={Style.icon} >
+                {icon}
+            </div>
+            <div className={Style.value}>
+                {value}
+            </div>
+        </div>
     )
 }
 
--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -3,6 +3,10 @@ import Style from './Contact.module.css'
 import { GrMail } from "react-icons/gr";
 import Button from "../../components/Button";
 import FormInput from "../../components/FormInput";
+import { AiOutlineMobile, AiFillHome } from "react-icons/ai";
+import { SiGmail } from "react-icons/si";
+import ContactInfo from '../../components/Card/ContactInfo';
+import SocialLogo from '../../components/SocialLogo';
 
 
 const index = ({ contactRef }) => {
@@ -19,39 +23,52 @@ const index = ({ contactRef }) => {
     return (
         <div ref={contactRef} className={Style.contact}>
             <div className={Style.footer}>
-                <div className={Style.mailBlock}>
-                    <GrMail size={30} color="#464444" className={Style.mail} />
-                    <a href="mailto:code.ashwin@gmail.com" className={Style.mailText}>
-                        code.ashwin@gmail.com
-                    </a>
+
+
+
+                <div className={Style.contactMe}>
+                    <p className={Style.contactMeText}>Find Me</p>
+                    <div className={Style.contactInfo}>
+                        <ContactInfo icon={<AiFillHome size={25} />} value={"Old Baneshwor, Kathmandu, Nepal"} />
+                        <ContactInfo icon={<AiOutlineMobile size={25} />} value="+977-1234567890" />
+                        <a href="mailto:code.ashwin@gmail.com" className={Style.mailText}>
+                            <ContactInfo icon={<SiGmail size={25} />} value="code.ashwin@gmail.com" />
+                        </a>
+                        <div className={Style.logo}>
+                            <SocialLogo />
+                        </div>
+                    </div>
                 </div>
 
-                <div className={Style.formDiv}>
-                    <form className={Style.form} onSubmit={handleSubmit}>
-                        <FormInput
-                            type="text"
-                            placeHolder="Enter your name here"
-                            value={form.name}
-                            name="name"
-                            onChange={handleChange}
-                        />
-                        <FormInput
-                            type="email"
-                            placeHolder="Enter your email here"
-                            value={form.email}
-                            name="email"
-                            onChange={handleChange}
-                        />
-                        <textarea
-                            placeholder="Enter your message here"
-                            className={Style.input}
-                            value={form.message}
-                            name="message"
-                            onChange={handleChange}
-                        />
-                        <br />
-                        <Button message="Send" />
-                    </form>
+                <div className={Style.contactForm}>
+                    <p className={Style.contactFormText}>Contact Me</p>
+                    <div className={Style.formDiv}>
+                        <form className={Style.form} onSubmit={handleSubmit}>
+                            <FormInput
+                                type="text"
+                                placeHolder="Enter your name here"
+                                value={form.name}
+                                name="name"
+                                onChange={handleChange}
+                            />
+                            <FormInput
+                                type="email"
+                                placeHolder="Enter your email here"
+                                value={form.email}
+                                name="email"
+                                onChange={handleChange}
+                            />
+                            <textarea
+                                placeholder="Enter your message here"
+                                className={Style.input}
+                                value={form.message}
+                                name="message"
+                                onChange={handleChange}
+                            />
+                            <br />
+                            <Button message="Send" />
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
