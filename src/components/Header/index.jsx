263c6e3168231a4dbdaf69101c8fe982b4f14247
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-04-17 14:13:16 +0545

    feat: add burger icon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Style from './Navbar.module.css'
 import Logo from '../Logo'
 import Menu from '../Menu'
+import BurgerButton from '../Button/BurgerButton'
 
 const index = (props) => {
     return (
-        <div className={Style.navItems}>
-            <Logo />
-            <Menu  {...props} />
-        </div>
+        <>
+            <div className={Style.navItems}>
+                <Logo />
+                <div className={Style.spacer} >
+                    <Menu  {...props} />
+                    <BurgerButton />
+                </div>
+            </div >
+        </>
     )
 }
 
