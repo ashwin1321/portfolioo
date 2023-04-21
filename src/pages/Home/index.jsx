0156b58dac394c03f1efd266fa429984e86ad935
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-04-21 12:54:53 +0545

    feat: add typing effect in the home section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Style from './Home.module.css'
 import SocialLogo from '../../components/SocialLogo'
 import Cv from '../../assets/cv/ashwinCV.pdf'
@@ -7,12 +7,28 @@ import { boy } from '../../assets/homeIcon'
 import Box from '../../components/HomeBox'
 
 const index = ({ homeRef }) => {
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+        let index = 0;
+        const intervalId = setInterval(() => {
+
+            setText(prevText => {
+                const name = 'Aashwin Khatiwada'
+                const letter = name.charAt(index)
+                return prevText + letter
+            });
+            index++;
+        }, 150);
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
         <div className={Style.home} ref={homeRef}>
             <div className={Style.homeSection}>
                 <div className={Style.content}>
                     <div className={Style.h1}>Hi, I am </div>
-                    <div className={Style.name} >Ashwin Khatiwada</div>
+                    <div className={Style.name} >{text}</div>
                     <div className={Style.h1}>Software Engineer Intern/ Relaxed</div>
                     <div className={Style.buttons}>
                         <a href="mailto: code.ashwin@gmail.com">
