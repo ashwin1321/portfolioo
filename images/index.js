c289df5e0c24199c41de129b88e50f9d033179c9
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-15 12:14:14 +0545

    Updates

---
+++ images/index.js
@@ -0,0 +1,12 @@
+import js from './js.png';
+import react from './react.png';
+import node from './node.png';
+import expense from './expense.png';
+import college from './college.jpg';
+import hospital from './hospital.svg';
+import paisa from './paisa.png';
+import crowd from './crowd.png';
+import boy from './boy.png'
+import spotify from './spotify.png';
+
+export { js, react, node, college, hospital, paisa, crowd, boy };
